95cf699b197d181fe014afbafecc7f3962796bc2
DanielRomo15 <137075635+DanielRomo15@users.noreply.github.com>
2025-06-13 07:51:20 -0500

    Update PuntoInteres.php

--- app/Models/PuntoInteres.php
+++ app/Models/PuntoInteres.php
@@ -9,14 +9,4 @@ class PuntoInteres extends Model
     // Nombre de la tabla en la base de datos
     protected $table = 'punto_interes';
 
-    // Campos que se pueden asignar de forma masiva
-    protected $fillable = [
-        'nombre',
-        'descripcion',
-        'categoria',
-        'imagen',
-        'latitud',
-        'longitud'
-    ];
-}
-
+    
