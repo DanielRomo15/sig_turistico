3a3f22ea02bad84198459e0699be24bfa343fa1b
DanielRomo15 <137075635+DanielRomo15@users.noreply.github.com>
2025-06-13 07:50:00 -0500

    Update PuntoInteresController.php

--- app/Http/Controllers/PuntoInteresController.php
+++ app/Http/Controllers/PuntoInteresController.php
@@ -1,4 +1,10 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\PuntoInteres;
+use Illuminate\Support\Facades\Storage;
 
 class PuntoInteresController extends Controller
 {
